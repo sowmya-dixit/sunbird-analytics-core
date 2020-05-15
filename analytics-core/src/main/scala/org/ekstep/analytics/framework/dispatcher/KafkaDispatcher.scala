86966f646beea03029111f995704fd3c0d6bd693
SowmyaDixit <sowmyadixit7@gmail.com>
2020-05-15 19:30:45 +0530

    Issue #0000 fix: Kafka config changes - remove compression

--- analytics-core/src/main/scala/org/ekstep/analytics/framework/dispatcher/KafkaDispatcher.scala
+++ analytics-core/src/main/scala/org/ekstep/analytics/framework/dispatcher/KafkaDispatcher.scala
@@ -88,7 +88,7 @@ object KafkaDispatcher extends IDispatcher {
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerList)
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
-        props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy")
+//        props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy")
         props.put(ProducerConfig.LINGER_MS_CONFIG, lingerMs)
         props
     }
